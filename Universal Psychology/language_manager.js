e9d25542d598296e7200c8ba35967ef5e5f55252
Jeddy939 <mrbowcock@gmail.com>
2025-09-15 13:31:47 +1000

    refactor: externalize upgrade text

--- Universal Psychology/language_manager.js
+++ Universal Psychology/language_manager.js
@@ -84,6 +84,162 @@ export const LanguageManager = {
       "GABA:",
       "Gamma-Aminobutyric Acid:"
     ],
+    upgrades: {
+      brainGrowth1: {
+        name: [
+          "Brain Growth: Stage 1",
+          "Brain Growth: Stage 1",
+          "Brain Growth: Stage 1",
+          "Brain Growth: Stage 1"
+        ],
+        desc: [
+          "Unlocks EASY Qs & Neuron Proliferation.",
+          "Unlocks EASY Qs & Neuron Proliferation.",
+          "Unlocks EASY Qs & Neuron Proliferation.",
+          "Unlocks EASY Qs & Neuron Proliferation."
+        ]
+      },
+      brainGrowth2: {
+        name: [
+          "Brain Growth: Stage 2",
+          "Brain Growth: Stage 2",
+          "Brain Growth: Stage 2",
+          "Brain Growth: Stage 2"
+        ],
+        desc: [
+          "Unlocks MEDIUM Qs & Hypothalamus.",
+          "Unlocks MEDIUM Qs & Hypothalamus.",
+          "Unlocks MEDIUM Qs & Hypothalamus.",
+          "Unlocks MEDIUM Qs & Hypothalamus."
+        ]
+      },
+      brainGrowth3: {
+        name: [
+          "Brain Growth: Stage 3",
+          "Brain Growth: Stage 3",
+          "Brain Growth: Stage 3",
+          "Brain Growth: Stage 3"
+        ],
+        desc: [
+          "Unlocks HARD Qs & Amygdala research.",
+          "Unlocks HARD Qs & Amygdala research.",
+          "Unlocks HARD Qs & Amygdala research.",
+          "Unlocks HARD Qs & Amygdala research."
+        ]
+      },
+      amygdalaActivation: {
+        name: [
+          "Activate Amygdala",
+          "Activate Amygdala",
+          "Activate Amygdala",
+          "Activate Amygdala"
+        ],
+        desc: [
+          "Doubles passive neuron production. WARNING: Random stimuli.",
+          "Doubles passive neuron production. WARNING: Random stimuli.",
+          "Doubles passive neuron production. WARNING: Random stimuli.",
+          "Doubles passive neuron production. WARNING: Random stimuli."
+        ]
+      },
+      nucleusAccumbens: {
+        name: [
+          "Nucleus Accumbens",
+          "Nucleus Accumbens",
+          "Nucleus Accumbens",
+          "Nucleus Accumbens"
+        ],
+        desc: [
+          "Unlocks access to NeuroGames.",
+          "Unlocks access to NeuroGames.",
+          "Unlocks access to NeuroGames.",
+          "Unlocks access to NeuroGames."
+        ]
+      },
+      prolifFactory: {
+        name: [
+          "Neuron Proliferation Factory",
+          "Neuron Proliferation Factory",
+          "Neuron Proliferation Factory",
+          "Neuron Proliferation Factory"
+        ],
+        desc: [
+          "Builds a facility for passive neuron growth (+0.5/sec).",
+          "Builds a facility for passive neuron growth (+0.5/sec).",
+          "Builds a facility for passive neuron growth (+0.5/sec).",
+          "Builds a facility for passive neuron growth (+0.5/sec)."
+        ]
+      },
+      dendriticSprouting: {
+        name: [
+          "Dendritic Sprouting",
+          "Dendritic Sprouting",
+          "Dendritic Sprouting",
+          "Dendritic Sprouting"
+        ],
+        desc: [
+          "Increase passive neuron production by 0.1%.",
+          "Increase passive neuron production by 0.1%.",
+          "Increase passive neuron production by 0.1%.",
+          "Increase passive neuron production by 0.1%."
+        ]
+      },
+      myelination: {
+        name: [
+          "Myelination",
+          "Myelination",
+          "Myelination",
+          "Myelination"
+        ],
+        desc: [
+          "Boost production but consumes more fuel and raises anxiety.",
+          "Boost production but consumes more fuel and raises anxiety.",
+          "Boost production but consumes more fuel and raises anxiety.",
+          "Boost production but consumes more fuel and raises anxiety."
+        ]
+      },
+      metabolicEfficiency: {
+        name: [
+          "Metabolic Efficiency",
+          "Metabolic Efficiency",
+          "Metabolic Efficiency",
+          "Metabolic Efficiency"
+        ],
+        desc: [
+          "Cuts Neurofuel consumption by 50%.",
+          "Cuts Neurofuel consumption by 50%.",
+          "Cuts Neurofuel consumption by 50%.",
+          "Cuts Neurofuel consumption by 50%."
+        ]
+      },
+      intermittentFasting: {
+        name: [
+          "Intermittent Fasting",
+          "Intermittent Fasting",
+          "Intermittent Fasting",
+          "Intermittent Fasting"
+        ],
+        desc: [
+          "Automatically purchase fuel every 10 seconds.",
+          "Automatically purchase fuel every 10 seconds.",
+          "Automatically purchase fuel every 10 seconds.",
+          "Automatically purchase fuel every 10 seconds."
+        ]
+      },
+      metabolicEfficiency2: {
+        name: [
+          "Metabolic Efficiency II",
+          "Metabolic Efficiency II",
+          "Metabolic Efficiency II",
+          "Metabolic Efficiency II"
+        ],
+        desc: [
+          "Further cuts fuel use by 50%.",
+          "Further cuts fuel use by 50%.",
+          "Further cuts fuel use by 50%.",
+          "Further cuts fuel use by 50%."
+        ]
+      }
+    },
     stats: {
       neurons: [
         "Neurons: {value}",
